e0cd8e271ebc8a7c84d09a1f3c6f46c97960c84d
shivanshhgit <shivanshbansal21@gmail.com>
2023-10-10 16:56:00 +0530

    Upload

---
+++ src/App.tsx
@@ -0,0 +1,34 @@
+import Charts from "./Components/Charts";
+
+function App() {
+  return (
+    <>
+      <Charts
+        location={"./72_machine_wise.csv"}
+        title={"PLANT 72 Machine wise for each year"}
+      ></Charts>
+      <Charts
+        location={"./72_year_wise.csv"}
+        title={"PLANT 72 Yearly for each machine"}
+      ></Charts>
+      <Charts
+        location={"./76_machine_wise.csv"}
+        title={"PLANT 76 Machine Wise for each year"}
+      ></Charts>
+      <Charts
+        location={"./76_year_wise.csv"}
+        title={"PLANT 76 Yearly for each machine"}
+      ></Charts>
+      <Charts
+        location={"./95_machine_wise.csv"}
+        title={"PLANT 95 Machine Wise for each year"}
+      ></Charts>
+      <Charts
+        location={"./95_year_wise.csv"}
+        title={"PLANT 95 Yearly for each machine"}
+      ></Charts>
+    </>
+  );
+}
+
+export default App;
---
+++ src/Components/Linechart.tsx
@@ -0,0 +1,72 @@
+import {
+  LineChart,
+  Line,
+  CartesianGrid,
+  XAxis,
+  YAxis,
+  Tooltip,
+  Legend,
+  ResponsiveContainer,
+} from "recharts";
+
+type Data = {
+  [key: string]: string | number;
+};
+type Values = {
+  data: Data[];
+};
+interface props {
+  values: Values;
+  title: string;
+}
+
+function Linechart({ values, title }: props) {
+  const renderLineChart = (
+    <ResponsiveContainer width="100%" height={400}>
+      <LineChart data={values.data} margin={{ right: 50, bottom: 40 }}>
+        <Line
+          name="TOTAL"
+          type="linear"
+          dataKey={(values.data[0] && Object.keys(values.data[0])[1]) || ""}
+          stroke="red"
+        />
+        <XAxis
+          axisLine={{ stroke: "red" }}
+          dataKey={(values.data[0] && Object.keys(values.data[0])[0]) || ""}
+          padding={{ left: 32, right: 32 }}
+          label={{
+            value: values.data[0] && Object.keys(values.data[0])[0],
+            position: "insideBottom",
+            offset: -20,
+            fill: "black",
+            fontWeight: "bold",
+          }}
+        />
+        <YAxis
+          axisLine={{ stroke: "red" }}
+          label={{
+            value: values.data[0] && Object.keys(values.data[0])[1],
+            position: "insideleft",
+            fill: "black",
+            angle: -90,
+            fontWeight: "bold",
+          }}
+        />
+        <Legend verticalAlign="top" align="right" />
+        <CartesianGrid stroke="#ccc" strokeDasharray="5 5" />
+        <Tooltip />
+      </LineChart>
+    </ResponsiveContainer>
+  );
+
+  return (
+    <>
+      <div className="box">
+        <h1>{title}</h1>
+        {renderLineChart}
+      </div>
+    </>
+  );
+}
+
+export default Linechart;
